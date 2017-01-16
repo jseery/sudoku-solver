0c248924d28bba36ad7ba6f6e792d9ec5dbe72b8
Jonathan Seery <seery.jonathan@gmail.com>
2017-01-16 17:58:55 -0500

    change Puzzle from namedtuple to Class. Move setup_puzzle to Puzzle.__init__ and add P Norvig's display as Puzzle.__repr__

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -4,31 +4,52 @@ from string import ascii_uppercase
 from string import digits
 from collections import namedtuple
 
+class Puzzle:
+
+    size=9
+    rows = ascii_uppercase[:size]
+    cols = digits[-size:]
+
+    def __init__(self):
+        """initializes puzzle structure with squares,
+        defines units and peers"""
+
+        self.squares = cross(self.rows, self.cols)
+
+        self.unitlist = ([cross(self.rows, c) for c in self.cols] +
+                    [cross(r, self.cols) for r in self.rows] +
+                    [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+
+        self.units = dict((s, [u for u in self.unitlist if s in u]) for s in self.squares)
+
+        self.peers = dict((s, set(sum(self.units[s], [])) - set([s])) for s in self.squares)
+
+    def import_state(self, grid):
+        """Convert grid into a dict of {square: char} with '0' or '.' for empties"""
+        chars = [c for c in grid if c in digits or c in '0.']
+        assert len(chars) == 81
+        self.values = dict(zip(self.squares, chars))
+
+        #store initial state of puzzle, but return working values dict
+        self.initial_state = self.values
+        
+        return self.values
+
+    def __repr__(self):
+        """Display these values as a 2-D grid."""
+        width = 1 + max(len(self.values[s]) for s in self.squares)
+        line = '+'.join(['-'*(width*3)]*3)
+        for r in self.rows:
+            print(''.join(self.values[r+c].center(width)+('|' if c in '36' else '') for c in self.cols))
+            if r in 'CF': print(line)
+
+
+
+
 def cross(A, B):
     """Cross product of elements in
     A and elements in B."""
     return [a + b for a in A for b in B]
-
-
-
-def setup_puzzle(size):
-
-    rows = ascii_uppercase[:size]
-    cols = digits[-size:]
-
-    squares = cross(rows, cols)
-
-    unitlist = ([cross(rows, c) for c in cols] +
-                [cross(r, cols) for r in rows] +
-                [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
-
-    units = dict((s, [u for u in unitlist if s in u]) for s in squares)
-
-    peers = dict((s, set(sum(units[s], [])) - set([s])) for s in squares)
-
-    Puzzle = namedtuple('Puzzle', field_names='squares, unitlist, units, peers, size')
-
-    return Puzzle(squares=squares, unitlist=unitlist, units=units, peers=peers, size=size)
 
 
 def test_puzzle(Puzzle):
@@ -49,5 +70,6 @@ def grid_to_values(grid, Puzzle):
     """Convert grid into a dict of {square: char} with '0' or '.' for empties"""
     chars = [c for c in grid if c in digits or c in '0.']
     assert len(chars) == 81
-    return dict(zip(Puzzle.squares, chars))
+    Puzzle.values = dict(zip(Puzzle.squares, chars))
+    return Puzzle
 
